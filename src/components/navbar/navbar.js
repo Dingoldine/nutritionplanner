27089edd68311d35bc806885b680999054596bef
Philip Rumman <philip@oc5767611354.ibm.com>
2019-03-16 11:09:34 +0100

    navbar update

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,21 +1,69 @@
 import React from 'react'
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
+import './navbar.css'
+import { NavLink as RRNavLink } from 'react-router-dom';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
 
+  export default class Navigator extends React.Component {
+    constructor(props) {
+      super(props);
+      this.toggle = this.toggle.bind(this);
+      this.state = {
+        isOpen: false
+      };
+    }
 
+    toggle() {
+      this.setState({
+        isOpen: !this.state.isOpen
+      });
+    }
 
-const navbar = props => {
-  return (
-<Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-    </Nav>
-  </Navbar>
-  );
+    render() {
+        return (
+          <div>
+            <Navbar color="light" light expand="md">
+              <NavbarBrand href="/">Nutrition Planner</NavbarBrand>
+              <NavbarToggler onClick={this.toggle} />
+              <Collapse isOpen={this.state.isOpen} navbar>
+                <Nav className="ml-auto" navbar>
+                  <NavItem>
+                    <NavLink tag={RRNavLink} exact to="/" href="/components/">Components</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink  href="https://gits-15.sys.kth.se/wwes/nutritionplanner">GitHub</NavLink>
+                  </NavItem>
+                  <UncontrolledDropdown nav inNavbar>
+                    <DropdownToggle nav caret>
+                      Options
+                    </DropdownToggle>
+                    <DropdownMenu right>
+                      <DropdownItem>
+                        Option 1
+                      </DropdownItem>
+                      <DropdownItem>
+                        Option 2
+                      </DropdownItem>
+                      <DropdownItem divider />
+                      <DropdownItem>
+                        Reset
+                      </DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+                </Nav>
+              </Collapse>
+            </Navbar>
+          </div>
+        );
+    }
 }
-
-
-export default navbar

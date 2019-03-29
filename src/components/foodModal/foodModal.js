0f6a663877e87acb3f5136ca70ead4490e67ff9c
williamwesterlund <william.e.westerlund@gmail.com>
2019-03-29 12:31:56 +0100

    modal done

--- src/components/foodModal/foodModal.js
+++ src/components/foodModal/foodModal.js
@@ -14,25 +14,39 @@ class FoodModal extends React.Component { // eslint-disable-line
 
     this.toggleDropDown = this.toggleDropDown.bind(this);
     this.toggleSplit = this.toggleSplit.bind(this);
+    this.handleDropdownSelected = this.handleDropdownSelected.bind(this);
 
     this.state = {
       nutrients: [],
       foodName,
       servingSelected: true,
+      servingWeightGrams: 0,
+      grams: 0,
       quantity: 1,
+      calories: 0,
+      protein: 0,
+      carbs: 0,
+      sugar: 0,
+      fats: 0,
       dropdownOpen: false,
       splitButtonOpen: false,
     }
   }
 
   onClick() {
-    const { foodName } = this.state
-    console.log(foodName)
-    if (this.state.nutrients.length === 0){
+    const { foodName, nutrients } = this.state
+    if (nutrients.length === 0){
       makeGetNutrientsRequest(foodName)      
       .then(res => {    
         this.setState({
           nutrients: res.foods[0],
+          servingWeightGrams: res.foods[0].serving_weight_grams,
+          grams: res.foods[0].serving_weight_grams,
+          calories: res.foods[0].nf_calories,
+          protein: res.foods[0].nf_protein,
+          carbs: res.foods[0].nf_total_carbohydrate,
+          sugar: res.foods[0].nf_sugars,
+          fat: res.foods[0].nf_total_fat
         })
       })
       .catch(err => {
@@ -42,22 +56,82 @@ class FoodModal extends React.Component { // eslint-disable-line
     }
   }
 
-  toggleDropDown() {
+  computeMacros() { // eslint-disable-line
+    const { nutrients, grams, servingWeightGrams } = this.state
     this.setState({
-      dropdownOpen: !this.state.dropdownOpen
+      calories: ((nutrients.nf_calories*grams)/servingWeightGrams).toFixed(2),
+      protein: ((nutrients.nf_protein*grams)/servingWeightGrams).toFixed(2),
+      carbs: ((nutrients.nf_total_carbohydrate*grams)/servingWeightGrams).toFixed(2),
+      sugar: ((nutrients.nf_sugars*grams)/servingWeightGrams).toFixed(2),
+      fat: ((nutrients.nf_total_fat*grams)/servingWeightGrams).toFixed(2)
+    })
+  }
+
+  handleAddFood() { // eslint-disable-line
+    console.log("food to be added")
+  }
+
+  handleInputChanged = async event => {
+
+    const { servingSelected, servingWeightGrams } = this.state
+    const { value } = event.target
+
+    this.setState({quantity: value})
+
+    if(servingSelected) {
+      this.setState({
+        grams: servingWeightGrams*value
+      }, () => {
+        this.computeMacros()
+      })
+    } else {
+      this.setState({
+        grams: value
+      }, () => {
+        this.computeMacros()
+      })
+    } 
+    
+    event.preventDefault();
+  }
+
+  handleDropdownSelected(type) {
+
+    const { quantity, servingWeightGrams } = this.state
+
+    if (type === 'serving') {
+      this.setState({
+        servingSelected: true,
+        grams: servingWeightGrams*quantity
+      }, () => {
+        this.computeMacros()
+      })
+    } else {
+      this.setState({
+        servingSelected: false,
+        grams: quantity
+      }, () => {
+        this.computeMacros()
+      })
+    }
+  }
+
+  toggleDropDown() {
+    const { dropdownOpen } = this.state
+    this.setState({
+      dropdownOpen: !dropdownOpen
     });
   }
 
   toggleSplit() {
+    const { splitButtonOpen } = this.state
     this.setState({
-      splitButtonOpen: !this.state.splitButtonOpen
+      splitButtonOpen: !splitButtonOpen
     });
   }
 
   render() {
-    const { nutrients, foodName } = this.state
-    
-    
+    const { nutrients, foodName, servingSelected, quantity, splitButtonOpen, calories, protein, carbs, sugar, fat } = this.state
     return(
       <Popup
       trigger={<div className="openModal"><div className="icon-container"><FaChevronRight /><FaChevronRight /></div></div>}
@@ -73,48 +147,48 @@ class FoodModal extends React.Component { // eslint-disable-line
         </a>
         <div className="modalHeader"> {foodName} </div>
         <div className="modalContent">
+        <div className="row justify-content-md-center">
           {' '}
           {console.log(nutrients)}
-          <Col sm="5">
+          <Col sm="4">
             <ListGroup>
-              <ListGroupItem>{this.state.servingSelected ? <p>{this.state.quantity} Serving ({nutrients.serving_unit})({nutrients.serving_weight_grams}g)</p> : <p>{this.state.quantity}g</p>}</ListGroupItem>
-              <ListGroupItem>Total Calories {nutrients.nf_calories}g</ListGroupItem>
-              <ListGroupItem>Protein {nutrients.nf_protein}g</ListGroupItem>
-              <ListGroupItem>Carbs {nutrients.nf_total_carbohydrate}g</ListGroupItem>
-              <ListGroupItem>Sugar {nutrients.nf_sugars}g</ListGroupItem>
-              <ListGroupItem>Fat {nutrients.nf_total_fat}g</ListGroupItem>
+              <ListGroupItem>{servingSelected ? <p>{quantity} Serving ({nutrients.serving_unit})({nutrients.serving_weight_grams}g)</p> : <p>{quantity}g</p>}</ListGroupItem>
+              <ListGroupItem>Calories {calories}g</ListGroupItem>
+              <ListGroupItem>Protein {protein}g</ListGroupItem>
+              <ListGroupItem>Carbs {carbs}g</ListGroupItem>
+              <ListGroupItem>Sugar {sugar}g</ListGroupItem>
+              <ListGroupItem>Fat {fat}g</ListGroupItem>
             </ListGroup>
           </Col>
 
-          <Col sm="5">
+          <Col sm="4">
             <InputGroup>
-              <Input value={this.state.quantity} onChange={e => { this.setState({quantity: e.target.value})}} />
-              <InputGroupButtonDropdown addonType="prepend" isOpen={this.state.splitButtonOpen} toggle={this.toggleSplit}>
-                <Button outline>{this.state.servingSelected ? <p>Serving ({nutrients.serving_weight_grams}g)</p> : <p>g</p>}</Button>
+              <Input value={quantity} onChange={e => { this.handleInputChanged(e) }}/>
+              <InputGroupButtonDropdown addonType="prepend" isOpen={splitButtonOpen} toggle={this.toggleSplit}>
+                <Button outline>{servingSelected ? <p>Serving ({nutrients.serving_weight_grams}g)</p> : <p>g</p>}</Button>
                 <DropdownToggle split outline />
                 <DropdownMenu>
-                  <DropdownItem onClick={() => {this.setState({servingSelected: true})}}>Serving {nutrients.serving_weight_grams}g</DropdownItem>
+                  <DropdownItem onClick={() => this.handleDropdownSelected('serving')}>Serving {nutrients.serving_weight_grams}g</DropdownItem>
                   <DropdownItem divider />
-                  <DropdownItem onClick={() => {this.setState({servingSelected: false})}}>Gram (1g)</DropdownItem>
-                </DropdownMenu>
+                  <DropdownItem onClick={() => this.handleDropdownSelected('gram')}>Gram (1g)</DropdownItem>
+                </DropdownMenu>          
               </InputGroupButtonDropdown>
             </InputGroup>
           </Col>
 
         </div>
+        </div>
         <div className="modalActions">
-          <Popup
-            trigger={<button className="modalButton"> Trigger </button>}
-            position="top center"
-            closeOnDocumentClick
+          <button
+            className="modalButton"
+            type="button"
+            onClick={() => {
+              console.log('food added ')
+              this.handleAddFood()
+            }}
           >
-            <span>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
-              magni omnis delectus nemo, maxime molestiae dolorem numquam
-              mollitia, voluptate ea, accusamus excepturi deleniti ratione
-              sapiente! Laudantium, aperiam doloribus. Odit, aut.
-            </span>
-          </Popup>
+            add
+          </button>
           <button
             className="modalButton"
             type="button"
